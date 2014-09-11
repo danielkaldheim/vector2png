603d5bb7b83411b9b9b4357321537f170ef7d734
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-09-11 16:00:42 +0200

    init commit

---
+++ ToAndroidSet.jsx
@@ -0,0 +1,80 @@
+/**********************************************************
+//	ToAndroidSet.jsx
+//	Created on 11.9.2014.
+//
+//	@author Daniel Rufus Kaldheim <daniel@idrift.no>
+//	@copyright 2012 - 2014 iDrift Web AS
+//	@version 1.0.0
+**********************************************************/
+
+#include "functions.jsxinc"
+
+var destFolder, sourceFolder, files, fileType;
+
+// Select the source folder.
+sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator files you want to convert', '~' );
+
+// If a valid folder is selected
+if ( sourceFolder != null ) {
+
+	files = new Array();
+	fileType = prompt( 'Select type of Illustrator files to you want to process. Eg: *.ai', ' ' );
+
+	// Get all files matching the pattern
+	files = sourceFolder.getFiles( fileType );
+
+	if ( files.length > 0 ) {
+
+		// Get the destination to save the files
+		destFolder = Folder.selectDialog( 'Select the folder where you want to save the converted files.', '~' );
+
+		for ( i = 0; i < files.length; i++ ) {
+
+			//file, resolution, filetypes, directory, options
+			drk_save.ToResolution(files[i], 56.25, ['png'], destFolder, {
+				directory: {
+					'subFolder': true,
+					'subFolderName' : 'drawable-ldpi'
+				}
+			});
+
+			drk_save.ToResolution(files[i], 75.0, ['png'], destFolder, {
+				directory: {
+					'subFolder': true,
+					'subFolderName' : 'drawable-mdpi'
+				}
+			});
+
+			drk_save.ToResolution(files[i], 100, ['png'], destFolder, {
+				directory: {
+					'subFolder': true,
+					'subFolderName' : 'drawable-hdpi'
+				}
+			});
+
+			drk_save.ToResolution(files[i], 150, ['png'], destFolder, {
+				directory: {
+					'subFolder': true,
+					'subFolderName' : 'drawable-xhdpi'
+				}
+			});
+
+			drk_save.ToResolution(files[i], 200, ['png'], destFolder, {
+				directory: {
+					'subFolder': true,
+					'subFolderName' : 'drawable-xxhdpi'
+				}
+			});
+
+		}
+
+		alert( 'Files are saved in ' + destFolder );
+	}
+	else {
+		alert( 'No matching files found' );
+	}
+}
+
+
+// End of file ToAndroidSet.jsx
+// Location: /Users/daniel/Documents/Illustrator Scripts/Vector/ToAndroidSet.jsx
---
+++ ToPNG.jsx
@@ -0,0 +1,47 @@
+/**********************************************************
+//	ToPNG.jsx
+//	Created on 11.9.2014.
+//
+//	@author Daniel Rufus Kaldheim <daniel@idrift.no>
+//	@copyright 2012 - 2014 iDrift Web AS
+//	@version 1.0.0
+**********************************************************/
+
+#include "functions.jsxinc"
+
+var destFolder, sourceFolder, files, fileType;
+
+// Select the source folder.
+sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator files you want to convert', '~' );
+
+// If a valid folder is selected
+if ( sourceFolder != null ) {
+
+	files = new Array();
+	fileType = prompt( 'Select type of Illustrator files to you want to process. Eg: *.ai', ' ' );
+
+	// Get all files matching the pattern
+	files = sourceFolder.getFiles( fileType );
+
+	if ( files.length > 0 ) {
+
+		// Get the destination to save the files
+		destFolder = Folder.selectDialog( 'Select the folder where you want to save the converted files.', '~' );
+
+		for ( i = 0; i < files.length; i++ ) {
+
+			//file, resolution, filetypes, directory, options
+			drk_save.ToResolution(files[i], 100.0, ['png'], destFolder, {});
+
+		}
+
+		alert( 'Files are saved in ' + destFolder );
+	}
+	else {
+		alert( 'No matching files found' );
+	}
+
+}
+
+// End of file ToPNG.jsx
+// Location: /Users/daniel/Documents/Illustrator Scripts/Vector/ToPNG.jsx
